c5a1bb01eeacbdae638cac6225d4a621a8c84471
saqibnisarsaqib <saqibnisarsaqib@gmail.com>
2025-09-26 23:43:34 +0500

    Add files via upload

---
+++ list_functions.py
@@ -0,0 +1,21 @@
+# Applying 5 list functions in Python
+
+# 1. append()
+cars = ["Suzuki", "Toyota", "Honda"]
+cars.append("Changan")
+print("After append:", cars)
+
+# 2. insert()
+cars.insert(1, "Kia")
+print("After insert:", cars)
+
+# 3. remove()
+cars.remove("Honda")
+print("After remove:", cars)
+
+# 4. sort()
+cars.sort()
+print("After sort:", cars)
+
+# 5. len()
+print("Length of list:", len(cars))
